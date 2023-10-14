77bd829f23f6d7538ba692812534a9cd8da549e7
iamsanjanarajkumar <iamsanjanarajkumar@gmail.com>
2023-10-14 17:25:33 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+require('dotenv').config()//env config
+const cors = require('cors')
+const PORT = 5000
+const connectDb = require('./db/config')
+
+const app = express()
+
+//middleware
+app.use(express.urlencoded({extended : true}))
+app.use(express.json())
+
+app.use(cors()) //cors middleware
+
+app.use(`/api/user`, require('./route/userRoute'))
+
+app.listen(PORT,() =>{
+    connectDb()
+    console.log(`server has started and running @ http://localhost:${PORT}`)
+})
+
